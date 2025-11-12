d17d363fc37f35ae6d0f4226f368ed61ee2a969e
M.BETTAIEB <bettaiebmedaly@gmail.com>
2025-11-12 10:48:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route("/health")
 def health():
-    return jsonify({"status":"OK"}), 200
+    return jsonify({"status": True})  # boolean True pour correspondre au test
 
-@app.route("/")
-def root():
-    return "Hello from my-app", 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    # Écoute sur le port 8888 pour Jenkins
+    app.run(host="127.0.0.1", port=8888)
