28fad3d0bcb5ab30dc870a2f3c9f2c7ed8a1a116
M.BETTAIEB <bettaiebmedaly@gmail.com>
2025-11-12 10:52:27 +0100

    Update test_health.py

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,6 +1,8 @@
 import requests
 
+BASE_URL = "http://127.0.0.1:8888"  # hardcodé pour Jenkins pipeline
+
 def test_health():
-    r = requests.get("http://localhost:8080/health", timeout=5)
+    r = requests.get(f"{BASE_URL}/health", timeout=5)
     assert r.status_code == 200
-    assert r.json().get("status") == "OK"
+    assert r.json().get("status") is True
